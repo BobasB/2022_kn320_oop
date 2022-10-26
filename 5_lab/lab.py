66cf82bb67bc995b70ba149a17c99b6f8210e2f6
Bohdan <bugil.bogdan@gmail.com>
2022-10-26 11:27:34 +0300

    update formats according to flake8

--- 5_lab/lab.py
+++ 5_lab/lab.py
@@ -1,32 +1,32 @@
 class Rocket:
-    def __init__(self, name:str, mass) -> None:
+    def __init__(self, name: str, mass) -> None:
         assert name.isascii(), "Значення name повинно містити символи таблиці ASCII"
         assert mass >= 0, "Масса Ракети має бути не 0!"
         self.name = name
         self.mass = mass
-        ## Можна ассерт замінити на IF, але це не дуже правильно
-        #if isinstance(mass, float):
+        # Можна ассерт замінити на IF, але це не дуже правильно
+        # if isinstance(mass, float):
         #    self.mass = mass
-        #else:
+        # else:
         #    raise AssertionError("Маса має бути введена цифрами")
-    
+
     @property
     def get_mass(self):
         return f"Ракета {self.name} має масу {self.mass} кг."
 
-    def convert_to_pounds(self): #конвертуємо кг в фунти
+    def convert_to_pounds(self):  # конвертуємо кг в фунти
         if self.mass > 0:
             return self.mass * 2.20462262
-        return None  
-    
+        return None
+
     def new_fun(self):
-        return f"Not tested"
+        return "Not tested"
+
 
 def test_pass():
     a = Rocket("Falcon 9", 549054.0)
     assert "Falcon 9" in a.get_mass, "Назва ракети не коректно виводиться"
-    #print()
-    #a.mass = -10
-    #print(a.convert_to_pounds())
-    #print(f"Маса ракети в фунтах {a.convert_to_pounds()}")
-
+    # print()
+    # a.mass = -10
+    # print(a.convert_to_pounds())
+    # print(f"Маса ракети в фунтах {a.convert_to_pounds()}")
--- 5_lab/tests.py
+++ 5_lab/tests.py
@@ -1,45 +1,47 @@
 import unittest
 from lab import Rocket
 
+
 class TestMyApp(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.name = "Ares I"
         cls.mass = 25400
-        
+
     def setUp(self) -> None:
         self.obj = Rocket(self.name, self.mass)
 
     def tearDown(self) -> None:
-        del self.obj #видаляти його не має змісту, просто для прикладу
+        del self.obj  # видаляти його не має змісту, просто для прикладу
 
     def test_if_object_created(self):
         self.assertEqual(self.obj.name, self.name)
         self.assertEqual(self.obj.mass, self.mass)
         self.assertIsInstance(self.obj, Rocket)
         self.assertNotIsInstance(self.obj, str)
-    
+
     def test_correct_output(self):
         self.assertIn(self.name, self.obj.get_mass)
         self.assertIn(str(self.mass), self.obj.get_mass)
-    
+
     def test_converter(self):
-        for mass in [20, 30.1, 50.356, 524588, 646.64646]: # Ми можемо передавати будб які числові значення
+        # Ми можемо передавати будб які числові значення
+        for mass in [20, 30.1, 50.356, 524588, 646.64646]:
             obj = Rocket(self.name, mass)
             self.assertAlmostEqual(obj.convert_to_pounds(), float(f"{mass * 2.20462262:.3f}"), 3)
-        for mass in [-10, 0]: # Пеервірка чи в процесі роботи хтось не змінив масу на відємну
+        for mass in [-10, 0]:  # Пеервірка чи в процесі роботи хтось не змінив масу на відємну
             self.obj.mass = mass
             self.assertIsNone(self.obj.convert_to_pounds())
-        
+
     def test_mass_greater_then_zero(self):
         with self.assertRaises(AssertionError):
-            obj = Rocket(self.name, -1)
+            Rocket(self.name, -1)
         self.assertIsInstance(Rocket(self.name, 0), Rocket)
-    
+
     def test_mass_as_a_string(self):
         with self.assertRaises(TypeError):
             Rocket(self.name, '152')
-           
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
