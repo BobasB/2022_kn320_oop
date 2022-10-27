68a3d2d4d8192ea44a278041eee8274bec1b8ea1
Bohdan <bugil.bogdan@gmail.com>
2022-10-27 14:28:30 +0300

    crene few steps with manual trigger

--- 4_lab/lab.py
+++ 4_lab/lab.py
@@ -1,5 +1,8 @@
 import os
 #os.environ["MY_VAR"] = "Я модифікував змінну"
-print(os.environ.get("MY_VAR"))
+try:
+    print(os.environ.get("MY_VAR"))
 
-print(os.environ["VAR_2_TEMP"])
+    print(os.environ["VAR_2_TEMP"])
+except:
+    print("Немає таких Енвайрмент змінних")
